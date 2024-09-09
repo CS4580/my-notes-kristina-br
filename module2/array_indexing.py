3fb8b285c33d3e64d4c28f41549ecd0c0613d992
Kristina Brinkman <kristinabrinkman@mail.weber.edu>
2024-09-09 12:41:56 -0600

    Finished class stuff 9/9

--- module2/array_indexing.py
+++ module2/array_indexing.py
@@ -18,6 +18,15 @@ def main():
     arr_2d = np.array([[21, 22, 23, 24],
               [31, 32, 33, 34],
               [41, 42, 43, 44]])
+    print(f'The 0,0 element {arr_2d[0,0]}')
+    print(f'The 2, -2 element {arr_2d[2,-2]}')
+    print(f'Full first row {arr_2d[0]}')
+
+    # slicing :p
+    arr_1d = np.arange(10)
+    
+
+
 
 
 if __name__ == '__main__':
